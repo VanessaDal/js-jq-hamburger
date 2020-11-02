163170d3b05bf2919d1d1e309cef75005b1dd2c8
Vanessa D'Alessandro <leavesthatfall@gmail.com>
2020-11-02 16:00:44 +0100

    done

---
+++ main.js
@@ -0,0 +1,10 @@
+// mostrare / nascondere il menu principale
+
+
+$(".header-right a").click(function() {
+    $(".hamburger-menu").addClass("active");
+  });
+
+  $(".close").click(function() {
+    $(".hamburger-menu").removeClass("active");
+  });
